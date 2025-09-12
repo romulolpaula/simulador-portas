f8c4568647b8acb805981d03fa054f16de1189b6
Mariah <mraiahh0409@gmail.com>
2025-09-12 19:34:43 -0300

    Criação da classe das portas

--- WpfApp1/Login.xaml.cs
+++ WpfApp1/Login.xaml.cs
@@ -23,5 +23,10 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+
+        private void btnEntrar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
