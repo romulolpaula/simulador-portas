a835a54b865c96cec296fea3af12901bf0a59082
Mariah <mraiahh0409@gmail.com>
2025-09-19 19:29:53 -0300

    parte visual com bolinhas

---
+++ WpfApp1/Controls/GateControl.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.Models;
+
+namespace WpfApp1.Controls
+{
+    /// <summary>
+    /// Interação lógica para GateControl.xam
+    /// </summary>
+    public partial class GateControl : UserControl
+    {
+        public GateModel Model { get; private set; }
+        public GateControl()
+        {
+            InitializeComponent();
+        }
+
+        public void Initialize(GateModel model, string imageFileName)
+        {
+            Model = model;
+            ImgGate.Source = new BitmapImage(new Uri($"pack://application:,,,/WpfApp1;component/Images/{imageFileName}"));
+            UpdatePortVisuals();
+        }
+
+        public void UpdatePortVisuals()
+        {
+            //pinta porta de verde se a saída for true, cinza se false
+            Input0.Fill = (Model.Inputs.Count > 0 && Model.Inputs[0].Output) ? Brushes.Green : Brushes.Gray;
+            Input1.Fill = (Model.Inputs.Count > 1 && Model.Inputs[1].Output) ? Brushes.Green : Brushes.Gray;
+            Output.Fill = Model.Output ? Brushes.Green : Brushes.Gray;
+        }
+
+        //evento para conectaar fios serão tratados no Form (ou janela) pai via bubbling
+        private void OutputPort_MouseDown(object sender, MouseButtonEventArgs e) => RaiseEvent(new RoutedEventArgs(OutputPortClickedEvent, this));
+
+        private void InputPort_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            int index = (sender == Input0) ? 0 : 1;
+            var args = new InputPortClickedEventArgs(InputPortClickedEvent, this) { InputIndex = index };
+            RaiseEvent(args);
+        }
+
+        public static readonly RoutedEvent OutputPortClickedEvent = EventManager.RegisterRoutedEvent(
+            "OutputPortClicked", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(GateControl));
+
+        public static readonly RoutedEvent InputPortClickedEvent = EventManager.RegisterRoutedEvent(
+            "InputPortClicked",RountingStrategy.Bubble,typeof(RoutedEventHandler),typeof(GateControl));
+    }
+}
--- WpfApp1/SimuladorCircuito.xaml.cs
+++ WpfApp1/SimuladorCircuito.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WpfApp1.Models;
+using WpfApp1.Services;
 
 namespace WpfApp1
 {
@@ -19,9 +21,21 @@ namespace WpfApp1
     /// </summary>
     public partial class SimuladorCircuito : Window
     {
+        private CircuitManager circuit = new CircuitManager();
+        private Type selectedGateType = null;
+        private Dictionary<GateModel, GateControl> modelToControl = new Dictionary<GateModel, GateControl>();
+        
         public SimuladorCircuito()
         {
             InitializeComponent();
+        }
+
+        private void cnvSimulador_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (selectedGateType == null) return;
+
+            var pos = e.GetPosition(cnvSimulador);
+            GateModel model = (GateModel)Activator.CreateInstance(selectedGateType);
         }
 
     }
